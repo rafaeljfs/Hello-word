a4bd317abf7ec218e63637faa4d87b12c584fdb2
09693799607 <rafaeljonasf@hotmail.com>
2015-05-29 19:13:17 -0300

    comentario exeplo

--- Hello world/src/Exercicios/exer.java
+++ Hello world/src/Exercicios/exer.java
@@ -4,7 +4,9 @@ public class exer {
 
 
 	public static <hello> void main(String[] args) {
-	System.out.println("hello word "  ) ;
+	System.out.println("hello word   "  ) ;
+	System.out.println(" rafael jonas  "  ) ;
+	//exeplo
 	}
 
 }
